a11bc9096ccd91cc00e70cdff96fc9ce0a2e1c8e
Harut <harut_intern@franqbox.com>
2020-07-26 01:06:39 +0400

    rerender to hooks

--- src/components/SongList.js
+++ src/components/SongList.js
@@ -1,35 +1,34 @@
-import React, { Component } from "react";
+import React from "react";
 import { connect } from "react-redux";
 import { selectSong } from "../actions/index";
 
-class SongList extends Component {
-  renderList = () => {
-    return this.props.songs.map((song) => {
+const SongList = ({ songs, selectSong }) => {
+  const renderList = () => {
+    return songs.map((song) => {
       return (
         <div className=" item" key={song.title}>
           <div className="right floated content">
-            <button 
-                className="ui button primary"
-                onClick = {() => this.props.selectSong(song)}
-            >Select</button>
+            <button
+              className="ui button primary"
+              onClick={() => selectSong(song)}
+            >
+              Select
+            </button>
           </div>
           <div className="content"> {song.title}</div>
         </div>
       );
     });
-  }
-  render() {
-    return <div className="ui divided list">{this.renderList()}</div>;
-  }
-}
+  };
+
+  return <div className="ui divided list">{renderList()}</div>;
+};
 
 const mapStateToProps = (state) => {
-  const { songs } = state
+  const { songs } = state;
   return { songs };
 };
 //we get our store's songs
 
-
-
-export default connect(mapStateToProps,{ selectSong })(SongList);
+export default connect(mapStateToProps, { selectSong })(SongList);
 //we convert our state's songs value to prop
